350cd3fa0e68b8867633a341cea715482700f1c9
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-02-21 21:44:26 +0630

    sorting list

---
+++ chapter-3-lists/cars.py
@@ -0,0 +1,10 @@
+cars=["bmw","audi","toyota","subaru"];
+print("Here is the original list:")
+print(cars);
+
+print("Here is the sorted list:")
+print(sorted(cars));
+
+cars.reverse();
+print(cars);
+print(len(cars));
---
+++ chapter-3-lists/places.py
@@ -0,0 +1,6 @@
+places = ["Liverpool","London","Newcastle","Barcelona"];
+
+print("Original list;")
+print(places)
+print("After sorting list:")
+print(sorted(places));
