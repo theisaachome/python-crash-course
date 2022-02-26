6b1906aa981de953a7392c529086f6e6d4a72225
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-02-26 13:04:52 +0630

    dictionary

--- chapter-5-if-statement/multi-block.py
+++ chapter-5-if-statement/multi-block.py
@@ -1,12 +1,13 @@
-from ast import Str
 
 
 age = 12;
-price = 0;
+
 if age < 4:
     price = 0
+    
 elif age < 18:
     price = 5;
+
 elif age < 65:
     price = 10;
 else:
---
+++ chapter-6-dictionary/alien.py
@@ -0,0 +1,12 @@
+alien_0={"color":"green","points":8};
+
+new_points = alien_0["points"];
+
+# add new key-value pairs
+
+alien_0["x_position"] = 0;
+alien_0["y_position"] = 100;
+print(alien_0["color"]);
+print("This alien hits " + str(new_points));
+
+print(alien_0);
